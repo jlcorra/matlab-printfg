f220c22fb6e770fd5be2ea36733ae03cb95007b4
José L. Corrales <jlcorrac@gmail.com>
2014-12-18 01:48:48 +0100

    Improve properties definition
    Change most properties fieldnames to better fit Matlab conventions (i.e.
    fig is now Figure, plot is now Line).

--- print_fig.m
+++ print_fig.m
@@ -1,4 +1,4 @@
-function print_fig(hfig, filename, format, size, style)
+function print_fig(hfig, filename, format, resize, properties)
 %PRINT_FIG Print figure to a file using specified format
 %
 %   Prints the figure with the handle hfig to the file filename at the 
@@ -6,28 +6,35 @@ function print_fig(hfig, filename, format, size, style)
 %   this function will try to apply the corresponding properties to every 
 %   plot, axis, label and legend object found under figure hfig.
 %
-%   PRINT_FIG(hfig, 'filename', 'format', [width, height])
-%   PRINT_FIG(hfig, 'filename', 'format', [width, height], style)
+%   PRINT_FIG(hfig, 'filename', format)
+%   PRINT_FIG(hfig, 'filename', format, [width, height])
+%   PRINT_FIG(hfig, 'filename', format, [width, height], properties)
 %
 %   Arguments:
 %       hfig    : Figure handle to print
 %       filename    : Filename without extension
-%       format  : Output format. See SAVEAS for supported formats
+%       format  : Output format. See SAVEAS or PRINT for supported formats
 %       width   : Printing width in points
 %       height  : Printing height in points
-%       style   : Struct array of figure properties to be overloaded
+%       properties  : Struct array of figure properties to be overloaded
 %
 %   Examples:
 %       print_fig(hf1, 'figure1', 'png', [350 275]);
-%       print_fig(hf2, 'figure2', 'epsc2', [350 275], style);
+%       print_fig(hf1, 'figure1', 'epsc2', [350 275]);
+%       print_fig(hf2, 'figure2', 'pdf', [350 275], style);
 %
 %   See also: SAVEAS, PRINT.
 
-% For the full copyright and license information, please view the LICENSE
-% file that was distributed with this source code.
+%   For the full copyright and license information, please view the LICENSE
+%   file that was distributed with this source code.
 %
-% @license http://opensource.org/licenses/MIT MIT License
-% @copyright Copyright (c) 2014 José L. Corrales
+%   @license http://opensource.org/licenses/MIT MIT License
+%   @copyright Copyright (c) 2014 José L. Corrales
+
+%% Arguments
+if nargin <= 2
+    error('Requires handle to figure, filename and format.');
+end
 
 %% Recover figure handles
 % Axis handles
@@ -41,58 +48,71 @@ hpl = findall(hfig, 'Type', 'Line');
 hlg = findall(hfig, 'Type', 'Axes', 'Tag', 'legend');
 %hlg = hlg(ismember(get(hlg, 'Interpreter'), {'latex'}));
 
-%% Default printing format
-print.fig.PaperUnits = 'points';
-print.fig.PaperPositionMode = 'manual';
-print.fig.PaperPosition = [0, 0, size(1), size(2)];
+%% Default properties
+% Some safe standard properties definitions that make plots look nice by
+% default. Overload as neccessary.
+prop.Axes.Box = 'off';
+prop.Axes.LineWidth = 1;
 
-print.axis.Box = 'off';
-print.axis.LineWidth = 1;
+prop.Line.LineWidth = 1;
+prop.Line.MarkerSize = 6;
 
-print.plot.LineWidth = 1;
-print.plot.MarkerSize = 6;
+%% Printing dimensions
+if length(resize) == 2
+    prop.Figure.PaperUnits = 'points';
+    prop.Figure.PaperPositionMode = 'manual';
+    prop.Figure.PaperPosition = [0, 0, resize(1), resize(2)];
+else
+    error(['Figure dimensions must be a 2-element vector containing ',...
+        'width and height.']);
+end
 
-%% Overload defaults with provided style
-f1 = {'fig', 'plot', 'axis', 'label', 'legend'};
+%% Overload default properties with user style
+f1 = {'Figure', 'Line', 'Axes', 'Label', 'Title', 'Legend'};
 % Dynamic fieldnames are only supported in R2014a and above
-if exist('style', 'var') && ~isempty(style)
-    for i = find(isfield(style, f1))
-        f2 = fieldnames(style.(f1{i}));
+if exist('properties', 'var') && ~isempty(properties)
+    for i = find(isfield(properties, f1))
+        f2 = fieldnames(properties.(f1{i}));
         for k = 1:length(f2)
-            print.(f1{i}).(f2{k}) = style.(f1{i}).(f2{k});
+            prop.(f1{i}).(f2{k}) = properties.(f1{i}).(f2{k});
         end
     end
 end
 
-%% Figure (printing size)
-if isfield(print, 'fig') && ~isempty(print.fig)
-    set(hfig, print.fig);
+%% Figure
+if isfield(prop, 'Figure') && ~isempty(prop.Figure)
+    set(hfig, prop.Figure);
 end
 
 %% Plot lines & markers
-if isfield(print, 'plot') && ~isempty(print.plot)
-    set(hpl, print.plot);
+if isfield(prop, 'Line') && ~isempty(prop.Line)
+    set(hpl, prop.Line);
 end
 
 %% Axis lines & ticks
 % [!] Axis font is also applied to legends
-if isfield(print, 'axis') && ~isempty(print.axis)
-    set(hax, print.axis);
+if isfield(prop, 'Axes') && ~isempty(prop.Axes)
+    set(hax, prop.Axes);
 end
 
-%% Axis labels & titles
-% Axis labels & title
-if isfield(print, 'label') && ~isempty(print.label)
-    set(cell2mat(get(hax, {'XLabel', 'YLabel', 'Title'})), print.label);
+%% Axis labels & title
+% Apply label properties to title, overload if neccessary
+if isfield(prop, 'Label') && ~isempty(prop.Label)
+    set(cell2mat(get(hax, {'XLabel', 'YLabel', 'Title'})), prop.Label);
+end
+if isfield(prop, 'Title') && ~isempty(prop.Title)
+    set(get(hax, 'Title'), prop.Title);
 end
 
 %% Legends
-if isfield(print, 'legend') && ~isempty(print.legend)
-    set(hlg, print.legend);
+if isfield(prop, 'Legend') && ~isempty(prop.Legend)
+    set(hlg, prop.Legend);
 end
 
 %% Remove additional whitespace margins
 % Undocumented property, might cause some issues with the LaTeX interpreter
+% Can be disabled through overloading, i.e.:
+%   properties.Axes.TightInset = get(gca, 'LooseInset')
 LooseInset = get(hax, {'LooseInset'});
 set(hax, {'LooseInset'}, get(hax, {'TightInset'}));
 
@@ -101,4 +121,5 @@ saveas(hfig, filename, format);
 
 %% Restore whitespace margins
 set(hax, {'LooseInset'}, LooseInset);
+
 end
